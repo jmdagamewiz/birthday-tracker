0e8286d9a967f92f5d1f48aceb163838cd587b54
jmdagamewiz <75661560+jmdagamewiz@users.noreply.github.com>
2021-02-28 21:40:09 +0800

    Create List All Window

---
+++ list_all_window.py
@@ -0,0 +1,100 @@
+from PyQt5.QtWidgets import QApplication, QWidget, QFrame, QLabel, QPushButton, QLineEdit, QRadioButton
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QGroupBox, QFormLayout
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import Qt
+import sys
+
+
+class PersonFrame(QFrame):
+
+    def __init__(self, name, birthday):
+        super().__init__()
+
+        self.name = name
+        self.birthday = birthday
+
+        self.init_frame()
+
+    def init_frame(self):
+        self.setFrameStyle(QFrame.StyledPanel)
+        self.create_frame_content()
+
+    def create_frame_content(self):
+
+        self.name_label = QLabel(self.name)
+        self.birthday_label = QLabel(self.birthday)
+        self.blank_label = QLabel("")
+
+        self.hbox = QHBoxLayout()
+        self.hbox.addWidget(self.name_label, stretch=2)
+        self.hbox.addWidget(self.birthday_label, stretch=2)
+        self.hbox.addWidget(self.blank_label, stretch=1)
+        self.setLayout(self.hbox)
+
+
+class ListAllGroupBox(QGroupBox):
+
+    def __init__(self):
+        super().__init__()
+
+        self.init_groupbox()
+
+    def init_groupbox(self):
+        self.setTitle("All Persons")
+        self.create_groupbox_content()
+
+    def create_groupbox_content(self):
+        self.person_frame1 = PersonFrame("Jay Hara", "October 1, 1000")
+
+        self.vbox = QVBoxLayout()
+        self.vbox.addWidget(self.person_frame1)
+
+        self.setLayout(self.vbox)
+
+
+class ListAllWindow(QWidget):
+    """
+    window for showing list of all persons and adding a person
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.title = "List All Window"
+
+        self.init_window()
+
+    def init_window(self):
+        self.setGeometry(100, 100, 420, 450)
+        self.setWindowTitle(self.title)
+        self.setWindowIcon(QIcon("assets/icon.png"))
+
+        self.create_window_content()
+
+    def create_window_content(self):
+
+        self.add_person_button = QPushButton("Add Person")
+
+        self.options_hbox = QHBoxLayout()
+        self.options_hbox.addStretch()
+        self.options_hbox.addWidget(self.add_person_button)
+
+        self.list_all_groupbox = ListAllGroupBox()
+
+        self.go_back_button = QPushButton("Go Back")
+        self.go_back_button.setFixedWidth(100)
+
+        self.vbox = QVBoxLayout()
+        self.vbox.addLayout(self.options_hbox)
+        self.vbox.addWidget(self.list_all_groupbox)
+        self.vbox.addStretch()
+        self.vbox.addWidget(self.go_back_button, alignment=Qt.AlignLeft)
+
+        self.setLayout(self.vbox)
+
+
+if __name__ == "__main__":
+    App = QApplication(sys.argv)
+    window = ListAllWindow()
+    window.show()
+    sys.exit(App.exec())
